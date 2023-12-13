0e63b00ad5ee1588dee888ab510c9f8bd1314ab2
Geoffrey Martelin <gemartel@z1r7p3.42lyon.fr>
2023-12-13 13:28:48 +0100

    clean

--- mini_talk/srcs/client.c
+++ mini_talk/srcs/client.c
@@ -1,58 +1,89 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   client.c                                           :+:      :+:    :+:   */
+/*   client_bonus.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gemartel <gemartel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/09 15:06:10 by gemartel          #+#    #+#             */
-/*   Updated: 2023/12/09 15:06:10 by gemartel         ###   ########.fr       */
+/*   Created: 2023/12/10 08:54:09 by gemartel          #+#    #+#             */
+/*   Updated: 2023/12/10 08:54:09 by gemartel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minitalk.h"
 
+int	g_bit_handler;
 
-void	find_bit(int pid, char c)
+void	validation_server(int signal)
+{
+	if (signal == SIGUSR2)
+	{
+		ft_printf("Message sent and received.\n");
+		exit(EXIT_SUCCESS);
+	}
+	else if (signal == SIGUSR1)
+		g_bit_handler = 1;
+}
+
+int	check_args(int argc, char **argv)
+{
+	pid_t	pid;
+	int		i;
+
+	if (argc != 3 || !argv[2][0])
+	{
+		ft_putstr_fd("Error arguments, please try again.\n", 2);
+		exit(EXIT_FAILURE);
+	}
+	pid = ft_atoi(argv[1]);
+	if (pid == 0)
+	{
+		ft_putstr_fd("Error: PID 0 is reserved for the kernel.\n", 2);
+		exit(EXIT_FAILURE);
+	}
+	i = -1;
+	while (argv[1][++i])
+	{
+		if (!(ft_isdigit(argv[1][i])) || pid < 0)
+		{
+			ft_putstr_fd("Error: a PID only has positive decimal numbers.", 2);
+			exit(EXIT_FAILURE);
+		}
+	}
+	return (pid);
+}
+
+void	send_bit(pid_t pid, unsigned char c)
 {
 	int	bit;
 
-	bit = 0;
-	while (bit < 8)
+	bit = __CHAR_BIT__ * sizeof(c) - 1;
+	while (bit >= 0)
 	{
-		if ((c & (1 << bit)))
+		g_bit_handler = 0;
+		if (c & (1 << bit))
 			kill(pid, SIGUSR1);
 		else
 			kill(pid, SIGUSR2);
-		usleep(100);
-		bit++;
+		bit--;
+		while (g_bit_handler != 1)
+			usleep(100);
 	}
-	
 }
 
 int	main(int argc, char **argv)
 {
-	int	pid;
-	int	i;
-	
+	pid_t	pid;
+	int		i;
 
-	if (argc != 3)
-	{
-		ft_putstr_fd("Usage: client <PID Serveur> <Message>\n", 2);
-		exit(EXIT_FAILURE);
-	}
-	i = 0;
-	pid = ft_atoi(argv[1]);
-	if (pid == -1)
-	{
-		ft_putstr_fd("Error: Pid serveur.\n", 2);
-		exit(EXIT_FAILURE);
-	}
-	while (argv[2][i])
-	{
-		find_bit(pid, argv[2][i]);
-		i++;
-	}
-	find_bit(pid, '\n');
-	return (0);
+	pid = check_args(argc, argv);
+	i = -1;
+	signal(SIGUSR1, validation_server);
+	signal(SIGUSR2, validation_server);
+	while (argv[2][++i])
+		send_bit(pid, argv[2][i]);
+	send_bit(pid, '\n');
+	send_bit(pid, 0);
+	while (1)
+		pause();
 }
--- mini_talk/srcs/server.c
+++ mini_talk/srcs/server.c
@@ -10,55 +10,53 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-
-
 #include "../include/minitalk.h"
 #include <stdio.h>
+#include <stdlib.h>
 
-void	handle_signal(int signal, siginfo_t *info, void *context)
+void	handler_signal(int signal, siginfo_t *info, void *context)
 {
-	static	int	c;
-	static	int	bit;
-	
+	static unsigned char	c = 0;
+	static int				bit = -1;
+
 	(void)context;
+	if (kill(info->si_pid, 0) < 0)
+	{
+		ft_printf("ERROR : cant send signal to pid: %d\n", info->si_pid);
+		exit(EXIT_FAILURE);
+	}
+	if (bit < 0)
+		bit = __CHAR_BIT__ * sizeof(c) - 1;
 	if (signal == SIGUSR1)
-		c |= (1 << bit);
-	bit++;
-	if (bit == 8)
-	{
-		ft_printf("%c", c);
-		c = 0;
-		bit = 0;
+		c |= 1 << bit;
+	else if (signal == SIGUSR2)
+		c &= ~(1 << bit);
+	if (!bit && c)
+		write(1, &c, 1);
+	else if (!bit && !c)
 		kill(info->si_pid, SIGUSR2);
-	}
+	bit--;
+	kill(info->si_pid, SIGUSR1);
 }
-int main(int argc, char **argv)
-{
-	int	pid;
-	struct sigaction	sig;
 
-	(void)argv;
-	if (argc != 1)
-	{
-		ft_putstr_fd("Error.\n", 2);
-		exit(EXIT_FAILURE);
-	}
+int	main(void)
+{
+	pid_t				pid;
+	struct sigaction	action;
+	sigset_t			signals;
+
 	pid = getpid();
-	sig.sa_sigaction = handle_signal;
-	sigemptyset(&sig.sa_mask);
-	sig.sa_flags = 0;
-	if (pid == -1)
-	{
-		ft_putstr_fd("Error getpid.\n", 2);
-		exit(EXIT_FAILURE);
-	}
-	printf("pid du processus = %d\n", pid);
-	
-	while (argc == 1)
-	{
-		sigaction(SIGUSR1, &sig, NULL);
-		sigaction(SIGUSR2, &sig, NULL);
+	sigemptyset(&signals);
+	sigaddset(&signals, SIGUSR1);
+	sigaddset(&signals, SIGUSR2);
+	action.sa_flags = SA_SIGINFO;
+	action.sa_mask = signals;
+	action.sa_handler = NULL;
+	action.sa_sigaction = handler_signal;
+	sigaction(SIGUSR1, &action, NULL);
+	sigaction(SIGUSR2, &action, NULL);
+	ft_printf("PID server -> %d\n", pid);
+	while (1)
 		pause();
-	}
 	return (0);
 }
